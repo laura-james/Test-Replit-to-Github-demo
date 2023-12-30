87133e667ddf7c0bc4bafeef7fd2c3adc7449fcf
laura-james <lollypopstar70@gmail.com>
2023-12-30 14:45:57 +0000

    More pylint fixes
    it didn't like 2 spaces or unused variables

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+"""
+Another docstring to get it past pylint
+"""
 def main():
-  """
-  This is an example docstring to describe my program
-  """
-  print("Hello World")
-  print("Goodbye world")
-  print("Hello World")
-  print("Goodbye world")
-  print("This is from GitHub!")
-  print("Its hot in B26")
-  print("Changing on 30/1/23")
-  a = "Hello"
-  b = "GoodbyE"
+    """
+    This is an example docstring to describe my program
+    """
+    print("Hello World")
+    print("Goodbye world")
+    print("Hello World")
+    print("Goodbye world")
+    print("This is from GitHub!")
+    print("Its hot in B26")
+    print("Changing on 30/1/23")
+    a = "Hello"
+    b = "GoodbyE"
+    print( a + b)
 
 main()
