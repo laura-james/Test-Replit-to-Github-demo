407607e6fc3138aafebf8c03580fcdcc827530b1
laura-james <lollypopstar70@gmail.com>
2023-12-30 15:57:55 +0000

    tweaked it so both tests should work now

--- tests.py
+++ tests.py
@@ -1,22 +1,25 @@
 """
 This  should be run by the GitHub Action
 """
+
+
 # content of test_sample.py
 def func(x):
-    """
+  """
     This  should be run by the GitHub Action
     """
-    return x + 1
+  return x + 1
 
-def test_answer_fail():
-    """
+
+def test_answer_success2():
+  """
     This  should be run by the GitHub Action
     """
-    assert func(3) == 5
+  assert func(3) == 4  # should pass now
+
 
 def test_answer_success():
-    """
+  """
     This  should be run by the GitHub Action
     """
-    assert func(4) == 5
-  
+  assert func(4) == 5
