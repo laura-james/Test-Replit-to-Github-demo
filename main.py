8cb870e62b3d5e2f13bac9c958dd26455efb9fcb
laura-james <lollypopstar70@gmail.com>
2023-12-30 15:46:51 +0000

    spacing

--- main.py
+++ main.py
@@ -21,11 +21,19 @@ main()
 
 # content of test_sample.py
 def func(x):
+    """
+    This  should be run by the GitHub Action
+    """
     return x + 1
 
 def test_answer_fail():
-
+    """
+    This  should be run by the GitHub Action
+    """
     assert func(3) == 5
     
 def test_answer_success():
+    """
+    This  should be run by the GitHub Action
+    """
     assert func(4) == 5
--- tests.py
+++ tests.py
@@ -3,11 +3,20 @@ This  should be run by the GitHub Action
 """
 # content of test_sample.py
 def func(x):
+    """
+    This  should be run by the GitHub Action
+    """
     return x + 1
 
 def test_answer_fail():
-
+    """
+    This  should be run by the GitHub Action
+    """
     assert func(3) == 5
 
 def test_answer_success():
+    """
+    This  should be run by the GitHub Action
+    """
     assert func(4) == 5
+  
