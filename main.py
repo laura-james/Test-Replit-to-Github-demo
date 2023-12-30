407f33b0c6eb73f79e936951efab4ac24fa657cf
laura-james <lollypopstar70@gmail.com>
2023-12-30 14:43:10 +0000

    Moved into a main() function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-"""
-This is an example docstring to describe my program
-"""
-print("Hello World")
-print("Goodbye world")
-print("Hello World")
-print("Goodbye world")
-print("This is from GitHub!")
-print("Its hot in B26")
-print("Changing on 30/1/23")
-a = "Hello"
-b = "GoodbyE"
+def main():
+  """
+  This is an example docstring to describe my program
+  """
+  print("Hello World")
+  print("Goodbye world")
+  print("Hello World")
+  print("Goodbye world")
+  print("This is from GitHub!")
+  print("Its hot in B26")
+  print("Changing on 30/1/23")
+  a = "Hello"
+  b = "GoodbyE"
+
+main()
